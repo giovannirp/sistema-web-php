955c6fe45b30cd5dc32a4267782cfd5caf90b5ff
giovanni <giovanni_ge@hotmai.com>
2025-12-07 17:58:05 -0300

    feat: adicionando codigo

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
     <nav class="navbar navbar-expand-lg bg-info">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Sistemas Web</a>
+        <a class="navbar-brand" href="#">Sistemas Web 3.0</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
